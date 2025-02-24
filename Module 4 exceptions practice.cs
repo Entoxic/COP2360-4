58798e97034cc6330f081da2920bded215345bc3
Malik A. <entoxify@gmail.com>
2025-02-23 21:07:08 -0500

    Update Module 4 exceptions practice.cs

--- Module 4 exceptions practice.cs
+++ Module 4 exceptions practice.cs
@@ -14,32 +14,32 @@ using System,
 
             try
             {
-                input1 number1 = Convert.ToInt32
-                input2 number2 = Convert.ToInt32
+                int number1 = Convert.ToInt32(input1);
+                int number2 = Convert.ToInt32(input2);
 
 
-                in result = Divide(number1, number2);
+                int result = Divide(number1, number2);
                 Console.WriteLine($"The result of {number1} divided by {number2} is: " + {result});
             }
             catch (FormatException ex)
             {
                 Console.WriteLine("Error: One or both of the inputs are not valid integers");
-                Console.WriteLine($"Detailed error message: {ex.message}");
+                Console.WriteLine($"Detailed error message: {ex.Message}");
             }
             catch (DivideByZeroException ex)
             {
                 Console.WriteLine("Error: Division by zero is not allowed");
-                Console.WriteLine($"Detailed error message: {ex.message}");
+                Console.WriteLine($"Detailed error message: {ex.Message}");
             }
             catch (Exception ex)
             {
                 Console.WriteLine("An unexpected error has occurred.");
-                Console.WriteLine($"Detailed error message: {ex.message}");
+                Console.WriteLine($"Detailed error message: {ex.Message}");
             }
             catch (OverflowException ex)
             {
                 Console.WriteLine("The numbers are too large try again.");
-                Console.WriteLine($"Detailed error message: {ex.message}");
+                Console.WriteLine($"Detailed error message: {ex.Message}");
             }
             
             
