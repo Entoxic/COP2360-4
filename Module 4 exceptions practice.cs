29c1b78ada871908b2c150c6a7eb412430e8fc71
Malik A. <entoxify@gmail.com>
2025-02-23 21:02:52 -0500

    Add files via upload
    Testing Module 4 Project

---
+++ Module 4 exceptions practice.cs
@@ -0,0 +1,59 @@
+using System,
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter 1st number: ");
+            string input1 = Console.ReadLine();
+
+            Console.Write("Enter 2nd number: ");
+            string input2 = Console.ReadLine();
+
+            try
+            {
+                input1 number1 = Convert.ToInt32
+                input2 number2 = Convert.ToInt32
+
+
+                in result = Divide(number1, number2);
+                Console.WriteLine($"The result of {number1} divided by {number2} is: " + {result});
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: One or both of the inputs are not valid integers");
+                Console.WriteLine($"Detailed error message: {ex.message}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed");
+                Console.WriteLine($"Detailed error message: {ex.message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error has occurred.");
+                Console.WriteLine($"Detailed error message: {ex.message}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("The numbers are too large try again.");
+                Console.WriteLine($"Detailed error message: {ex.message}");
+            }
+            
+            
+                Console.WriteLine("Press any key to exit program.");
+                Console.ReadKey();
+            
+        }
+
+        
+            static int Divide(int a, int b)
+        {
+                return a / b;
+        }    
+ 
+    }   
+
+}
